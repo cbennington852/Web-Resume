eb0e3517074ae928957f45534c8b025d357c8a1e
Charlie <45837976+cbennington852@users.noreply.github.com>
2024-12-13 18:50:35 -0800

    mold done

---
+++ sketch.js
@@ -0,0 +1,41 @@
+// Set up canvas
+
+ let d; let molds = []; let moldNum = 7000;
+ let canvasSize = 900;
+let circleX, circleY;
+function setup() {
+    canvas = createCanvas(canvasSize, canvasSize);
+    canvas.parent("canvas-container"); // Attach to the div
+    angleMode(DEGREES);
+
+    d = pixelDensity();
+
+
+    for (let i = 0; i < moldNum; i++) {
+
+        molds[i] = new mold((i)%canvasSize,(i)%canvasSize);
+    }
+
+
+}
+
+
+function draw() {
+    background(0,5);
+    loadPixels();
+
+    for (let i = 0; i < moldNum; i++) {
+        molds[i].display();
+        molds[i].update();
+    }
+}
+
+// Called whenever the mouse is pressed
+function mousePressed() {
+    // Check if the mouse is inside the canvas
+    if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+        console.log("Mouse clicked at:", mouseX, mouseY);
+        fill(255, 0, 0);
+        ellipse(mouseX, mouseY, 200, 200); // Draw a red circle where clicked
+    }
+}
