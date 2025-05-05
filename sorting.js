ac96c3425e4321585d303b3c5d23cbc4de2a4999
Charlie <45837976+cbennington852@users.noreply.github.com>
2025-05-04 19:12:34 -0700

    sortingjs addon

---
+++ sorting.js
@@ -0,0 +1,59 @@
+let cols; let rows;
+let size = 15;
+let c = [];
+
+function setup() {
+    createCanvas(800, 500);
+    cols = width/size;
+    rows = height/size;
+    for (let i=0; i<cols; i++){
+        c[i] = [];
+        for (let j=0; j<rows; j++){
+            c[i][j] = color(random(256), random(256), random(256));
+        }
+    }
+    // print(c);
+}
+
+function draw() {
+    background(220);
+    for (let i=0; i<cols; i++){
+        for (let j=0; j<rows; j++){
+            fill(c[i][j]);
+            rect(i*size, j*size, size, size);
+        }
+        //change the color.
+        bubbleSortPass(c[i]);
+        sleep(100);
+        //also sort horizantally.
+    }
+
+    //get which rect the mouse is on, and randomise those pixels
+    let locX = floor(mouseX / size);
+    let locY = floor(mouseY / size);
+    console.log(locX, locY);
+    console.log(typeof locY, typeof locX);
+    if (!(locX < 1 || locY > (rows-1) || locY < 1 || locX > (cols-1))){
+        c[locX][locY] = color(random(256), random(256), random(256));
+        c[locX+1][locY] = color(random(256), random(256), random(256));
+        c[locX+1][locY+1] = color(random(256), random(256), random(256));
+        c[locX][locY+1] = color(random(256), random(256), random(256));
+        c[locX-1][locY-1] = color(random(256), random(256), random(256));
+
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function bubbleSortPass(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        if (green(arr[i]) > green(arr[i + 1])) {
+            // Swap arr[i] and arr[i + 1]
+            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        }
+    }
+    return arr;
+}
