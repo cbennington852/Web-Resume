5ed8d4e5de0a437b8c7ad60a24831ac7d1d7ea6a
Charlie <45837976+cbennington852@users.noreply.github.com>
2025-05-06 17:24:55 -0700

    clouds

---
+++ boxes.js
@@ -0,0 +1,83 @@
+let cols; let rows;
+let size = 20;
+let c = [];
+
+function setup() {
+    if (getDeviceType() === 'mobile') {
+        createCanvas(600, 800);
+    }
+    else {
+        createCanvas(800, 500);
+    }
+
+    cols = width/size;
+    rows = height/size;
+    for (let i=0; i<cols; i++) {
+        c[i] = [];
+        for (let j = 0; j < rows; j++) {
+            c[i][j] = color(random(256), random(256), random(256));
+        }
+    }
+}
+
+function draw() {
+    background(220);
+    for (let i=0; i<cols; i++){
+        for (let j=0; j<rows; j++){
+            fill(c[i][j]);
+            square(i*size, j*size, size);
+        }
+        //change the color.
+        bubbleSortPass(c[i]);
+
+        //also sort horizantally.
+    }
+
+    //get which rect the mouse is on, and randomise those pixels
+    let locX = floor(mouseX / size);
+    let locY = floor(mouseY / size);
+    console.log(locX, locY);
+    console.log(typeof locY, typeof locX);
+    if (!(locX < 1 || locY > (rows-1) || locY < 1 || locX > (cols-2))){
+        c[locX][locY] = color(random(256), random(256), random(256));
+        c[locX+1][locY] = color(random(256), random(256), random(256));
+        c[locX][locY+1] = color(random(256), random(256), random(256));
+        c[locX+1][locY+1] = color(random(256), random(256), random(256));
+        c[locX-1][locY-1] = color(random(256), random(256), random(256));
+        c[locX][locY-1] = color(random(256), random(256), random(256));
+        c[locX-1][locY] = color(random(256), random(256), random(256));
+        c[locX-1][locY+1] = color(random(256), random(256), random(256));
+
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getDeviceType() {
+    const userAgent = navigator.userAgent.toLowerCase();
+
+    if (/(tablet|ipad)/i.test(userAgent)) {
+        return "tablet";
+    } else if (
+        /(android|iphone|ipod|blackberry|windows phone)/i.test(userAgent)
+    ) {
+        return "mobile";
+    } else {
+        return "desktop";
+    }
+}
+
+function bubbleSortPass(arr) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let val = hue(arr[i]);
+        let valNext = hue(arr[i+1]) ;
+        if (val > valNext) {
+            // Swap arr[i] and arr[i + 1]
+            [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        }
+    }
+    return arr;
+}
